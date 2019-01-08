6d00f0fbc0546ce3e5e340f61cd842c234460f99
Breanna Baltaxe-Admony <breanna@Breannas-MacBook-Pro.local>
2019-01-07 21:42:42 -0800

    automation!

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+#update lambda
+zip study_chat.zip main.py
+aws lambda update-function-code --function-name="study_chat" --zip-file="fileb:///Users/breanna/Documents/Research/study_girl/study_chat.zip" || exit 1
+rm study_chat.zip
+
+#push to git
+git add .
+git commit
+git push
+
+
