66b58e21b2b86ad2f14bd3fd16208e2bd0dec115
Breanna Baltaxe-Admony <breanna@Breannas-MacBook-Pro.local>
2019-01-09 22:34:18 -0800

    appending chat messages to file

--- main.py
+++ main.py
@@ -34,29 +34,36 @@ with open('messages.txt','a+') as cfile:
         time.sleep(waittime)
 """
 def lambda_handler(event, context):
-    tstring = "it's a test!\n"
+
+    #---S3 setup---#
     bucketname = "study-girl"
     filename = "chat.txt"
     s3_path = "study-girl/" + filename
-    
     s3 = boto3.resource('s3')
     object = s3.Object(bucketname,s3_path)
     
     #download file from s3
     s3.Bucket(bucketname).download_file(s3_path,"/tmp/chat.txt")
     
+    #---get data---#
+    request = requests.get('https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=Cg0KC2hIVzFvWTI2a3hR&part=snippet&key=AIzaSyAA9XB1_C35bXonayY8SlPEG3ahUl9-BZw')
+    # Parse return text using json.loads function. Returns dict
+    response_dictionary = json.loads(request.text)
+
+
+
+
     #Read from a file
     #with open('/tmp/chat.txt','r') as cfile:
     #    response = cfile.read()
    
     #append file
     with open('/tmp/chat.txt','a+') as cfile:
-        cfile.write('hiya\n')
-
-
+        for event_dict in response_dictionary['items']:
+            cfile.write(event_dict['snippet']['publishedAt']+' '+ json.dumps(event_dict['snippet']['textMessageDetails']['messageText'], indent=5)+'\n')
+    
     #write file to s3
     object.put(Body=open('/tmp/chat.txt','rb'))
     
     return "eof"
-
 
